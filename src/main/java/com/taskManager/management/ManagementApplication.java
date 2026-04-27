5480fcacdb21565ede8e86d073baeaf11fd599a0
priyanka patel <pn.priyankapatel@gmail.com>
2026-04-27 12:40:00 +0530

    first commit

---
+++ src/main/java/com/taskManager/management/ManagementApplication.java
@@ -0,0 +1,13 @@
+package com.taskManager.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementApplication.class, args);
+	}
+
+}
